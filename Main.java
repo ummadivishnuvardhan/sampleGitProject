87dd679bc279e7a060328764e45dbe722b7420f6
Ummadi Vishnu Vardhan <ummadivishnuvardhan46@gmail.com>
2026-03-10 20:20:10 +0530

    Update and rename index.html to Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    // Method to perform a task
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        // Program starts here
+        System.out.println("Program started...");
+
+        // Call a method
+        greet("Vishnu");
+
+        // End of program
+        System.out.println("Program ended.");
+    }
+}
